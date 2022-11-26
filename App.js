89a7c9f9a14928bb4ac516b363ed19a25a8eba66
lusqua <lucasaguiar.floripa@gmail.com>
2022-11-26 18:54:47 -0300

    Feat. progresso

--- App.js
+++ App.js
@@ -1,13 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
+import { BleManager, Device } from 'react-native-ble-plx';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start workkkkkkkkkkkkking on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { useRef } from 'react';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.manager = new BleManager();
+    this.state = {
+      devices: [],
+    };
+  }
+
+  scan() {
+    this.manager.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      console.log(device.name);
+      this.setState({ devices: [...this.state.devices, device] });
+      this.manager.stopDeviceScan();
+    });
+  }
+
+  render() {
+    this.manager.onStateChange((state) => {
+      if (state === 'PoweredOn') {
+        console.log('BLE is powered on');
+        this.scan();
+      }
+    }, true);
+    return (
+      <View style={styles.container}>
+        <Text>Funcionou muito</Text>
+        {/* list with devices */}
+        {this.state.devices.map((device) => (
+          <Text>{device.name}</Text>
+        ))}
+        <StatusBar style="auto" />
+      </View>
+    );
+  };
 }
 
 const styles = StyleSheet.create({
